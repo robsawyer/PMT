52404321ec0a8afcee340712d02582ee72b8acce
Rob Sawyer <rob.sawyer@razorfish.com>
2012-04-24 11:04:37 -0700

    Corrected some conflicts

--- app/models/production_manager.php
+++ app/models/production_manager.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 class ProductionManager extends AppModel {
 	var $name = 'ProductionManager';
@@ -40,39 +39,4 @@ class ProductionManager extends AppModel {
 	);
 
 }
-=======
-<?php
-class ProductionManager extends AppModel {
-	var $name = 'ProductionManager';
-	var $displayField = 'fullname';
-	
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
-
-	var $validate = array(
-		'fullname' => array(
-							'rule' => 'isUnique',
-							'message' => 'This produciton manager is already in the database.'
-							) 
-	);
-	
-	var $hasAndBelongsToMany = array(
-		'Project' => array(
-			'className' => 'Project',
-			'joinTable' => 'production_managers_projects',
-			'foreignKey' => 'production_manager_id',
-			'associationForeignKey' => 'project_id',
-			'unique' => true,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'finderQuery' => '',
-			'deleteQuery' => '',
-			'insertQuery' => ''
-		)
-	);
-
-}
->>>>>>> c568480e3497dffdc91a3a55e4719ca190238935
 ?>
